0faa32a3a59dad4dfbbcf060b06ca22cfa83248c
Mariel Westervelt <elle.westervelt@gmail.com>
2018-09-23 19:14:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`)
 }
-function happyHolidaysTo(holiday) {
+function happyHolidayTo(holiday) {
   return (`Happy ${holiday}, ${name}!`)
 }
